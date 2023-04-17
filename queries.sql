ef0f5e01a44ea52e99d10efe0a220c99dee33e0a
raammnath025 <raamnath025@gmail.com>
2023-04-17 18:56:41 -0400

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -18,3 +18,33 @@ FROM (
 ) t
 WHERE rn = 1 AND release_year <= 2022 AND release_year >= 2012
 ORDER BY release_year, imdb_votes DESC;
+
+-- Based on the number of IMDB votes, does Netflix or Paramount have higher viewership?
+SELECT SUM(imdb_votes) from netflix
+SELECT COUNT(imdb_votes) from netflix
+SELECT SUM(imdb_votes) from paramount
+SELECT COUNT(imdb_votes) from paramount
+
+-- Between 2012 and 2022, how many releases were there per year? 
+SELECT COUNT(id), release_year
+FROM paramount
+GROUP BY release_year
+ORDER BY COUNT (id) DESC;
+
+
+SELECT COUNT(id), release_year
+FROM netflix
+GROUP BY release_year
+ORDER BY COUNT (id) DESC;
+
+
+CREATE TABLE combined as
+	SELECT * FROM paramount
+		UNION
+	SELECT * FROM netflix;
+
+
+SELECT COUNT(id), release_year
+FROM combined
+GROUP BY release_year
+ORDER BY COUNT (id) DESC;
